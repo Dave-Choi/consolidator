e5bb44786969c1ca0868afa8b5d3541eebd9de4f
Rails Dev <railsdev@MacBookProBot.local>
2013-04-10 14:51:42 -0400

    Expand comments

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,14 @@
 
     Trusted users would then be able to see a catalog of things
     you're willing to lend, for example.
+
+    Friendships are directed relationships, meaning that there's
+    a source user (user_id in the schema), and a target user 
+    (friend_id in the schema).
+
+    Sharing should be enabled when a friendship is bidirectional,
+    but User records can be initialized when a User claims someone
+    has their stuff and initiates the friendship.
 =end
 
 class Friendship < ActiveRecord::Base
