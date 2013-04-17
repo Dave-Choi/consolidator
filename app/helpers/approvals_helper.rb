2c07473854a0c0084ab8092e77a364339e8e41fe
Rails Dev <railsdev@MacBookProBot.local>
2013-04-17 19:37:28 -0400

    Add BorrowRequest and Approval model views.

--- app/helpers/approvals_helper.rb
+++ app/helpers/approvals_helper.rb
@@ -1,2 +1,12 @@
 module ApprovalsHelper
+    def status_icon(status)
+        # Using icons from Twitter Bootstrap
+        icon_map = {
+            'pending' => 'icon-time',
+            'approved' => 'icon-ok',
+            'rejected' => 'icon-remove'
+        }
+
+        return ('<i class="' + icon_map[status] + '"></i>').html_safe
+    end
 end
