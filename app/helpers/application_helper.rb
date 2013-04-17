bca0d3aca1fa0d9180782e792941fd0925f8da0b
Rails Dev <railsdev@MacBookProBot.local>
2013-04-17 19:50:57 -0400

    Move status_icon helper from Approvals to Application level

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def status_icon(status)
+    # Using icons from Font-Awesome
+        icon_map = {
+            'pending' => 'icon-time',
+            'approved' => 'icon-ok',
+            'rejected' => 'icon-remove'
+        }
+
+        return ('<i class="' + icon_map[status] + '"></i>').html_safe
+    end
 end
--- app/helpers/approvals_helper.rb
+++ app/helpers/approvals_helper.rb
@@ -1,12 +1,2 @@
 module ApprovalsHelper
-    def status_icon(status)
-        # Using icons from Twitter Bootstrap
-        icon_map = {
-            'pending' => 'icon-time',
-            'approved' => 'icon-ok',
-            'rejected' => 'icon-remove'
-        }
-
-        return ('<i class="' + icon_map[status] + '"></i>').html_safe
-    end
 end
