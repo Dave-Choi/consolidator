276501d763b73b4e5267592df299082124ee3dfd
Rails Dev <railsdev@MacBookProBot.local>
2013-04-17 19:36:22 -0400

    Add note about a callback that should be added

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -3,6 +3,8 @@
     Things.
 
     Status should be one of "accepted", "rejected", or "pending" (default).
+
+    TODO: The record's timestamps should be updated whenever the status is updated
 =end
 
 class Approval < ActiveRecord::Base
