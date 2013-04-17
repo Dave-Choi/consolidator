bbd9e0d4b348fa25c5e808d94fe10cab445e3709
Rails Dev <railsdev@MacBookProBot.local>
2013-04-17 03:12:42 -0400

    Add comments to explain purpose

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -1,3 +1,10 @@
+=begin
+    Approvals represent per-owner responses to requests to borrow
+    Things.
+
+    Status should be one of "accepted", "rejected", or "pending" (default).
+=end
+
 class Approval < ActiveRecord::Base
   belongs_to :borrow_request
   belongs_to :user
--- app/models/borrow_request.rb
+++ app/models/borrow_request.rb
@@ -1,5 +1,27 @@
+=begin
+    When a User wants to borrow a Thing, a BorrowRequest is
+    created, which represents the intent to borrow the item.
+
+    When the request is created, it should spawn Approval records
+    to ping each owner for their OK to borrow the item.
+        - If any Approval is rejected, the BorrowRequest fails.
+        - If all Approvals are accepted, the BorrowRequest succeeds,
+        and a transfer should be initiated.
+=end
+
 class BorrowRequest < ActiveRecord::Base
   belongs_to :thing
   belongs_to :user
-  # attr_accessible :title, :body
+
+  def create_approvals()
+    # Creates one approval for each User with a stake in the Thing
+    # to be borrowed
+    owners = self.thing.users
+    owners.each do |owner|
+        new_approval = Approval.new()
+        new_approval.borrow_request = self
+        new_approval.user = owner
+        new_approval.save!
+    end
+  end
 end
