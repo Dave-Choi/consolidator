556628c586d4cc41c43607732ac132f2fb0e8158
Rails Dev <railsdev@MacBookProBot.local>
2013-04-29 10:12:49 -0400

    Add JQuery Masonry and basic styling for boxes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.masonry.min.js
 //= require bootstrap
 //= require_tree .
+
+
+$(function(){
+    $('.masonry').imagesLoaded(function(){
+        this.masonry();
+    });
+});
