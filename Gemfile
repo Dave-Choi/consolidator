08f6278cabe55b8179f4122c0c3eb4900331567d
Rails Dev <railsdev@MacBookProBot.local>
2013-05-02 07:54:15 -0400

    Add Amazon S3 for production

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'devise'
 gem 'amistad'
 gem 'simple_form'
 gem 'paperclip', '~> 3.0'
+gem 'aws-sdk'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,14 @@ Consolidator::Application.configure do
   config.action_mailer.default_url_options = { :host => 'http://rocky-mountain-3186.herokuapp.com/' }
   # Required for Devise on Heroku
   config.assets.initialize_on_precompile = false
+
+  # Amazon Web Services S3 config for Paperclip
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
