df76653f37d51f142727c0d44954e7ee12a72d13
Rails Dev <railsdev@MacBookProBot.local>
2013-05-02 09:32:04 -0400

    Add paperclip production default for :url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Consolidator::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url => ":s3_domain_url"
   }
 end
